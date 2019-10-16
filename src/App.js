8236035a162e73410bf711331bfbb16ca0f10739
Ashrf Ali <ashrftvm1@gmail.com>
2019-10-16 19:12:51 +0400

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <div className="App">
-      <>Hello from App</>
-    </div>
+    <>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms" exact component={Rooms} />
+      <Route path="/single-room" exact component={SingleRoom} />
+    </>
   );
 }
 
