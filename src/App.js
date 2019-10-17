289fb7c8e83467a3c55e20feb1d278fa01ed6015
Ashrf Ali <ashrftvm1@gmail.com>
2019-10-17 13:13:13 +0400

    navbar switch and links completed

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms" exact component={Rooms} />
-      <Route path="/single-room" exact component={SingleRoom} />
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" exact component={Rooms} />
+        <Route path="/rooms/:slug" exact component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
